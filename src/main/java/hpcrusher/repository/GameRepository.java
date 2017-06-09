d3c2265cfaf083ff0726b0668f733427cb50ccf7
Hpcrusher <liebl_david@yahoo.com>
2017-06-09 23:27:13 +0200

    Fix: Query nach Feature einbaut falsch

--- src/main/java/hpcrusher/repository/GameRepository.java
+++ src/main/java/hpcrusher/repository/GameRepository.java
@@ -15,7 +15,7 @@ import java.util.UUID;
  */
 public interface GameRepository extends JpaRepository<Game, UUID>, JpaSpecificationExecutor<Game> {
 
-    @Query(value = "SELECT g FROM Game g WHERE ((player1 = :p1 OR player2 = :p2) AND p1Winner IS NULL) OR player2 IS NULL")
+    @Query(value = "SELECT g FROM Game g WHERE ((player1 = :p1 OR player2 = :p2) AND  winner = 0) OR player2 IS NULL")
     List<Game> findByPlayer1OrPlayer2OrPlayer2IsNull(@Param("p1") Person player1, @Param("p2") Person player2);
 
 }
